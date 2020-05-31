e218acd87a9016a6499a7883e7df5cf52f3b9d17
jhansi dunuka <dunukajhansi02@gmail.com>
2020-05-31 20:52:32 +0200

    created a branch multiplication

--- main.py
+++ main.py
@@ -13,50 +13,28 @@ def index():
             <form method="post">
                 A: <input type="text" name="A" />
                 B: <input type="text" name="B" />
-                <p><input type="submit" name="operator" value="Add" />
-                <input type="submit" name="operator" value="Sub" />
-                <input type="submit" name="operator" value="Mul" />
+                <p>
+                <input type="submit" name="operator" value="Div" />
             </form>
         '''
     elif request.method == 'POST':
         # calculate result
         a = request.form.get('operator')
-        if a == 'Add':
+  
+        if a == 'Div':
             A = request.form.get('A')
             B = request.form.get('B')
-            return redirect(url_for('add', A=A, B=B))
-        if a == 'Sub':
-            A = request.form.get('A')
-            B = request.form.get('B')
-            return redirect(url_for('sub', A=A, B=B))
-        if a == 'Mul':
-            A = request.form.get('A')
-            B = request.form.get('B')
-            return redirect(url_for('mul', A=A, B=B)) 
+            return redirect(url_for('div', A=A, B=B))
 
 
- @app.route('/add')
-def add():
+
+@app.route('/div')
+def div():
     dict = request.args.to_dict()
     A = eval(dict['A'])
     B = eval(dict['B'])
-    result = A+B
+    result = A/B
     return 'result: %s' % result
-    @app.route('/sub')
-def sub():
-    dict = request.args.to_dict()
-    A = eval(dict['A'])
-    B = eval(dict['B'])
-    result = A-B
-    return 'result: %s' % result
-    @app.route('/mul')
-def mul():
-    dict = request.args.to_dict()
-    A = eval(dict['A'])
-    B = eval(dict['B'])
-    result = A*B
-    return 'result: %s' % result
-
 
 # run app
 if __name__ == '__main__':
