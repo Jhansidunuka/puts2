37a474187251cc5206b382a02cb5e359ffe9b206
jhansi dunuka <dunukajhansi02@gmail.com>
2020-05-31 21:48:02 +0200

    created test case for multiplication

--- test.py
+++ test.py
@@ -4,36 +4,25 @@ from cal import app
 class MyTestCase(unittest.TestCase):
     def test_index(self):
         with app.test_client() as a:
-            get_index = a.get('/')
-            self.assertEqual(get_index._status_code, 200)
-            post_index_add = a.post('/', data={'A':'6', 'B':'5', 'operator': 'Add'})
-            post_index_sub = a.post('/', data={'A':'6', 'B':'5', 'operator': 'Sub'})
-            post_index_mul = a.post('/', data={'A':'6', 'B':'5', 'operator': 'Mul'})
-            post_index_div = a.post('/', data={'A':'6', 'B':'5', 'operator': 'Div'})
-            self.assertEqual(post_index_add._status_code, 302)
-            self.assertEqual(post_index_sub._status_code, 302)
-            self.assertEqual(post_index_mul._status_code, 302)
-            self.assertEqual(post_index_div._status_code, 302)
+            getindex = a.get('/')
+            self.assertEqual(getindex.statuscode, 200)
+           
+            postindexmul = a.post('/', data={'A':'4', 'B':'2', 'operator': 'Mul'})
+        
+            
+            self.assertEqual(postindexmul.statuscode, 302)
+           
 
-    def test_add(self):
-        with app.test_client() as a:
-            get_add = a.get('/add', query_string={'A':'6', 'B':'5'})
-            self.assertEqual(get_add._status_code, 200)
-            result_string = get_add.get_data(as_text=True)
-            result = eval(result_string.split('result: ')[1])
-            self.assertEqual(result, 6)
 
-    def test_sub(self):
+
+    def test_mul(self):
         with app.test_client() as a:
-            getsub = a.get('/sub', query_string={'A':'4', 'B':'2'})
-            self.assertEqual(getsub.statuscode, 200)
-            resultstring = getsub.get_data(as_text=True)
+            getmul = a.get('/mul', query_string={'A':'4', 'B':'2'})
+            self.assertEqual(getmul.statuscode, 200)
+            resultstring = getmul.get_data(as_text=True)
             result = eval(resultstring.split('result: ')[1])
-            self.assertEqual(result, 1)
-
-    
+            self.assertEqual(result, 8)
 
   
-
 if __name__ == '__main__':
     unittest.main()
